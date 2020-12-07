7412129878f8d20f3561815bd4e6cbf528ae1c76
Steve Todorov <steve.todorov@carlspring.com>
2020-12-07 02:07:22 +0200

    issue-1967: Move JMeter installation into a separate script.

--- common-scripts/setup-jdk.sh
+++ common-scripts/setup-jdk.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 # The environment variables are set in the jdk file.
 # If you need to debug - just copy them here.
 
+export | grep -E '(JAVA_.+|JDK_.+|GLIBC_.+|PATH)='
+
 # Creating JDK home
 mkdir -p ${JAVA_HOME}
 ln -s ${JAVA_HOME} /java/jdk
@@ -58,13 +60,3 @@ java -version
 echo "Testing javac installation..." > /dev/null;
 javac -version
 
-echo "Installing jmeter" > /dev/null
-cd /java
-curl --fail --speed-time 15 --speed-limit 1024000 -O -J -L "http://apache.cbox.biz/jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz"
-echo "${JMETER_CHECKSUM} apache-jmeter-${JMETER_VERSION}.tgz" | sha512sum -c -
-tar zxf apache-jmeter-${JMETER_VERSION}.tgz
-ln -s /java/apache-jmeter-${JMETER_VERSION}/bin/{jmeter,jmeter-server} /usr/local/bin/
-rm -rfv "/java/apache-jmeter-${JMETER_VERSION}/"*docs*
-echo "Testing jmeter installation..." > /dev/null;
-jmeter --version
-rm -rf apache-jmeter-${JMETER_VERSION}.tgz
